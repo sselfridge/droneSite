ee963fdca69873e4acf6769f50fd07330826c6ed
Sam Selfridge <sirclesam@yahoo.com>
2016-10-12 22:45:22 -0600

    On Going test to get swiper to work for my main slide show.
    Things are going well so far.

---
+++ rocky/swiper/swiperTest.js
@@ -0,0 +1,47 @@
+
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    pagination: '.swiper-pagination',
+    onSlideChangeStart: slideChange,
+    // scrollbarHide: false,
+    paginationClickable: true,
+    keyboardControl: true
+    // scrollbar: '.swiper-scrollbar'
+  })
+});
+
+
+$(".statusBox").css("background","red");
+
+function slideChange(swiper){
+  // console.log(swiper);
+  // console.log(swiper.activeIndex);
+  var index = swiper.activeIndex;
+  console.log(swiper.slides[index]);
+  var vid = swiper.slides[index].querySelector("#video");
+  if(vid){
+    console.log("We're on video!");
+    vid.play();
+    $(".statusBox").css("background","yellow");
+  }else{
+    $(".statusBox").css("background","orange");
+  }
+}
+
+
+
+// get the video
+var video = document.querySelector('#video');
+// use the whole window and a *named function*
+window.addEventListener('touchstart', function videoStart() {
+  video.play();
+  console.log('first touch');
+  // remove from the window and call the function we are removing
+  this.removeEventListener('touchstart', videoStart);
+});
