a450b2cb0a1ed7f8db04828e2c344c7a99663270
Sam Selfridge <sirclesam@yahoo.com>
2016-10-14 10:53:21 -0600

    Adding untracked file for swiper and bootstrap.

---
+++ meinswiper.js
@@ -0,0 +1,49 @@
+"use strict";
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    pagination: '.swiper-pagination',
+    onSlideChangeStart: slideChange,
+    // scrollbarHide: false,
+    paginationClickable: true,
+    keyboardControl: true
+    // scrollbar: '.swiper-scrollbar'
+  })
+});
+
+
+$(".statusBox").css("background","red");
+
+function slideChange(swiper){
+  var index = swiper.activeIndex;
+  var vid = swiper.slides[index].querySelector("#video");
+  if(vid){
+    // console.log("We're on video!");
+    vid.play();
+    $(".statusBox").css("background","yellow");
+  }else{
+    $(".statusBox").css("background","orange");
+  }
+}
+
+// Android fix
+// Using touchstart will play the video as soon as the screen is touched.
+// get the video
+var video = document.querySelector('#video');
+// use the whole window and a *named function*
+window.addEventListener('touchstart', function videoStart() {
+  video.play();
+  // console.log('first touch');
+  // remove from the window and call the function we are removing
+  this.removeEventListener('touchstart', videoStart);
+});
+
+
+//iphone fix
+// youtube code:
+// <div class="videoWrapper"><iframe src="https://www.youtube.com/embed/2kbdn804h7s" frameborder="0" allowfullscreen></iframe></div>
---
+++ rockysky.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var navList = "Tom";
+var ralph = 1234;
+
+$.get("logo.html",function(data){
+  $("#logoPlaceholder").replaceWith(data);
+  console.log("Replace Logo");
+});
+
+$.get("navbar.html",function(data){
+  $("#navbarPlaceholder").replaceWith(data);
+  selectActiveNav();
+});
+
+// Called After loading the navbar
+// Removes active from all navs then assign it to one where the ID is in the URL
+// Possible problem with partial matches.
+function selectActiveNav(){
+  var loc = window.location.pathname;
+  $(".navbar .navbar-nav > li").each(function() {
+    // console.log("ID:" + $(this).attr('id'));
+    $(this).removeClass("active");
+    if ( loc.match($(this).attr('id'))) {
+      $(this).addClass("active");
+    }
+  });
+}
+
+function findBootstrapEnvironment() {
+    var envs = ['xs', 'sm', 'md', 'lg'];
+    var $el = $('<div>');
+    $el.appendTo($('body'));
+
+    for (var i = envs.length - 1; i >= 0; i--) {
+        var env = envs[i];
+
+        $el.addClass('hidden-'+env);
+        if ($el.is(':hidden')) {
+            $el.remove();
+            return env;
+        }
+    }
+}
+
+$("#info").text(findBootstrapEnvironment());
+$("#screen").text($(document).height() + "--" + $(window).height());
+$("#screen2").text($(document).width() + "--" + $(window).width());
+
+$(window).resize(function(){
+  $("#info").text(findBootstrapEnvironment());
+  $("#screen").text($(document).height() + "--" + $(window).height());
+  $("#screen2").text($(document).width() + "--" + $(window).width());
+});
